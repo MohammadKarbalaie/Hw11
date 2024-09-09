477f56e12d39c7935e9cbe94d7c0a7f63779bf9f
Mohammad karbalaiemehdi <mohammadkarbalaiemehdi@outlook.com>
2024-09-09 20:03:57 +0330

    create-login-signup-authresponse-signupresponse-project-js-with-typescript-front-end

--- Typescript-project-js/apis/services/auth.service.ts
+++ Typescript-project-js/apis/services/auth.service.ts
@@ -1,24 +1,34 @@
-import { urls } from "../url";
-import { httpClient } from "../client";
+import {urls} from "../url.ts"
+import { httpClient } from '../client.ts';  
+  
+interface LoginCredentials {  
+  username: string;  
+  password: string;  
+}  
+  
+interface AuthResponse {  
+  token: string;  
+  expiration: number;  
+} 
 
-interface LoginData {
-  // Define the structure of login data
+interface SignupCredentials {  
+  username: string;  
+  password: string;  
+}   
+interface SignupResponse {  
+  user: {  
+   username: string;  
+   id: number;  
+  };  
+  token: string;  
+}  
+  
+export async function login(data: LoginCredentials): Promise<AuthResponse> {  
+  const response = await httpClient().post(urls.auth.login, data);  
+  return response.data;  
 }
 
-interface SignupData {
-  // Define the structure of signup data
-}
-
-interface AuthResponse {
-  // Define the structure of the response data
-}
-
-export async function login(data: LoginData): Promise<AuthResponse> {
-  const response = await httpClient().post<AuthResponse>(urls.auth.login, data);
-  return response.data;
-}
-
-export async function signup(data: SignupData): Promise<AuthResponse> {
-  const response = await httpClient().post<AuthResponse>(urls.auth.signup, data);
-  return response.data;
+export async function signup(data: SignupCredentials): Promise<SignupResponse> {  
+  const response = await httpClient().post(urls.auth.signup, data);  
+  return response.data;  
 }
